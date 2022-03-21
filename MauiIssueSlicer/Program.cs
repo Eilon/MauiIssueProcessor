b464794a0688a94529079f44ae9dc6e42efe496d
Eilon Lipton <Eilon@users.noreply.github.com>
2022-03-20 22:05:55 -0700

    Add weekly open/close summary logic

--- MauiIssueSlicer/Program.cs
+++ MauiIssueSlicer/Program.cs
@@ -4,8 +4,29 @@ using Newtonsoft.Json;
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-var dataPath = @"C:\Users\elipton\Downloads\export.csv";
+var dataPath = @"C:\Users\elipton\Downloads\maui-issues-all.csv";
 var csvTable = new DataTableBuilder().ReadCsv(dataPath);
+
+var milestoneMapping = new Dictionary<string, string>()
+{
+    { "7770503", "6.0.300-rc.1" },
+    { "7617522", "6.0.300-preview.14" },
+    { "6929376", ".NET 7" },
+    { "7665569", "Future" },
+    { "7526548", "6.0.200-preview.13" },
+    { "6954947", "6.0.100-rc.1" },
+    { "7286786", "6.0.200-preview.11" },
+    { "7194018", "6.0.101-preview.10" },
+    { "7280520", "6.0.200-preview.12" },
+    { "7016119", "6.0.101-preview.9" },
+    { "6904587", "6.0.100-preview.7" },
+};
+
+// These milestones aren't used yet
+//{ "", "6.0.300" },
+//{ "", "6.0.300-servicing" },
+//{ "", "6.0.100-preview.6" },
+
 
 Console.WriteLine($"Loaded {csvTable.NumRows} rows of data");
 
@@ -25,6 +46,7 @@ foreach (var csvIssueRow in csvTable.Rows)
 {
     var labels = JsonConvert.DeserializeObject<GitHubLabel[]>(csvIssueRow.Values[labelsColumnIndex]);
 
+    milestoneMapping.TryGetValue(csvIssueRow.Values[milestoneIdColumnIndex], out var resolvedMilestoneName);
 
     var strongIssueRow = new IssueRow()
     {
@@ -35,16 +57,36 @@ foreach (var csvIssueRow in csvTable.Rows)
         IsOpen = csvIssueRow.Values[stateColumnIndex] == "open",
         PrimaryArea = labels.FirstOrDefault(l => l.name.StartsWith("area/", StringComparison.Ordinal))?.name,
         IsBug = labels.Any(l => l.name == "t/bug"),
-        MilestoneName = csvIssueRow.Values[milestoneIdColumnIndex],
+        MilestoneName = resolvedMilestoneName,
     };
 
     strongIssueRows.Add(strongIssueRow);
 }
 
 var startDate = new DateTimeOffset(2022, 1, 1, 0, 0, 0, TimeSpan.Zero);
-var daysSinceStartDate = DateTimeOffset.Now - startDate;
+startDate = strongIssueRows.Min(x => x.CreatedAt).Date;
 
+var daysSinceStartDate = DateTimeOffset.Now - startDate;
 var weeks = (int)Math.Ceiling(daysSinceStartDate.TotalDays / 7d);
+
+var weekSpan = new TimeSpan(days: 7, hours: 0, minutes: 0, seconds: 0);
+
+var openClosedByWeek = new List<OpenClosedItem>();
+
+for (int i = 0; i < weeks; i++)
+{
+    var fromDate = (startDate + i * weekSpan).Date;
+    var toDate = (fromDate + weekSpan).Date;
+
+    var issuesOpenedInRange = strongIssueRows.Count(i => i.CreatedAt.Date >= fromDate && i.CreatedAt.Date < toDate);
+    var issuesClosedInRange = strongIssueRows.Count(i => i.ClosedAt.HasValue && i.ClosedAt.Value.Date >= fromDate && i.ClosedAt.Value.Date < toDate);
+
+    openClosedByWeek.Add(new OpenClosedItem { Week = fromDate, IssuesOpened = issuesOpenedInRange, IssuesClosed = issuesClosedInRange });
+}
+
+var openClosedTable = new DataTableBuilder().FromEnumerable(openClosedByWeek);
+openClosedTable.SaveCSV(@"C:\Users\elipton\Downloads\maui-issues-all-openclosed-by-week.csv");
+
 
 Console.WriteLine("Done");
 
@@ -52,6 +94,13 @@ Console.WriteLine("Done");
 int GetColumnIndex(IEnumerable<(string First, int Second)> columnNamesByIndex, string columnName)
 {
     return columnNamesByIndex.Single(c => c.First == columnName).Second;
+}
+
+class OpenClosedItem
+{
+    public DateTime Week { get; set; }
+    public int IssuesOpened { get; set; }
+    public int IssuesClosed { get; set; }
 }
 
 class IssueRow
